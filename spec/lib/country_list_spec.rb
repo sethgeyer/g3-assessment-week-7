46d41ea90d16100231a0f1d9926072d0247a0869
Seth Geyer <seth.geyer@gmail.com>
2014-07-21 11:35:47 -0600

    refactored countries_spec

--- spec/lib/country_list_spec.rb
+++ spec/lib/country_list_spec.rb
@@ -2,22 +2,19 @@ require 'spec_helper'
 
 describe CountryList do
 
-  it "can return an array of countries" do
-    country_list = CountryList.new
+  let(:country_list) { CountryList.new }
 
+  it "can return an array of countries" do
     expect(country_list.all).to be_kind_of(Array)
     expect(country_list.all.length).to be > 0
   end
 
   it "can return an array of continents" do
-    country_list = CountryList.new
-
     expect(country_list.continents).to eq(["Africa", "Europe", "North America", "South America"])
   end
 
   describe "#countries_for_continent" do
     it "returns an array of countries associated with the related continent" do
-      country_list = CountryList.new
       countries = country_list.countries_for_continent("Africa")
       expect(countries.length).to eq(2)
       countries = country_list.countries_for_continent("North America")
